0cb9f6b9540c7b915d376b9d7a4a7af6c7ff5a91
nam0101 <nam.nv205106@gmail.com>
2024-07-11 12:15:48 +0700

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    //ksp
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22"
+}
+
+
+android {
+    namespace = "nv.nam.screencastingwebrtc"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "nv.nam.screencastingwebrtc"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0 SNAPSHOT"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.webrtc)
+    implementation(libs.gson)
+    implementation(libs.java.websocket)
+    implementation(libs.koin.android)
+}
